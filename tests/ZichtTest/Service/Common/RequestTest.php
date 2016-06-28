fe8c44dac11acc2f4c803fe0ae6a7e392b459934
Boudewijn Schoon <boudewijn@zicht.nl>
2016-06-28 12:30:29 +0000

    unittest dependencies and CS
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtServiceWrapper/trunk@86674 0f35748b-8228-47f0-bbea-889afbbd9a42

--- tests/ZichtTest/Service/Common/RequestTest.php
+++ tests/ZichtTest/Service/Common/RequestTest.php
@@ -5,22 +5,24 @@ namespace ZichtTest\Service\Common;
 use PHPUnit_Framework_TestCase;
 use Zicht\Service\Common\Request;
 
-class RequestTest extends PHPUnit_Framework_TestCase {
-    function testGetParameterDeep() {
+class RequestTest extends PHPUnit_Framework_TestCase
+{
+    function testGetParameterDeep()
+    {
         $value = rand(1, 9999);
         $request = new Request('', array(array('a' => array('b' => array('c' => $value)))));
         $this->assertEquals($value, $request->getParameterDeep(array(0, 'a', 'b', 'c')));
     }
 
-
-    function testGetParameterDeepObjects() {
+    function testGetParameterDeepObjects()
+    {
         $value = rand(1, 9999);
         $request = new Request('', array((object)array('a' => (object)array('b' => (object)array('c' => $value)))));
         $this->assertEquals($value, $request->getParameterDeep(array(0, 'a', 'b', 'c')));
     }
 
-
-    function testSetParameterDeep() {
+    function testSetParameterDeep()
+    {
         $value = rand(1, 9999);
         $value2 = rand(10000, 19999);
         $request = new Request('', array(array('a' => array('b' => array('c' => $value)))));
@@ -28,8 +30,8 @@ class RequestTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals($value2, $request->getParameterDeep(array(0, 'a', 'b', 'c')));
     }
 
-
-    function testSetParameterDeepIfValueNotPreviouslySet() {
+    function testSetParameterDeepIfValueNotPreviouslySet()
+    {
         $value2 = rand(10000, 19999);
         $request = new Request('', array());
         $request->setParameterDeep(array(0, 'a', 'b', 'c'), $value2);
