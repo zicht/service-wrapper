2c89a30da3770057dc83394699f1cf6846dd62c6
Gerard van Helden <gerard@zicht.nl>
2016-11-07 11:00:47 +0100

    fix tests and CS

--- src/Zicht/Service/Common/ServiceObserver.php
+++ src/Zicht/Service/Common/ServiceObserver.php
@@ -7,6 +7,8 @@
 namespace Zicht\Service\Common;
 
 /**
+ * Base interface for observers; replaced by `ServiceObserverInterface`
+ *
  * @deprecated Use ServiceObserverInterface in stead.
  */
 interface ServiceObserver extends ServiceObserverInterface
