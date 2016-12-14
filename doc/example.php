86d7e62cf10e5f8d409ec61a56cb87aea587ca22
Gerard van Helden <gerard@zicht.nl>
2016-12-14 14:50:09 +0100

    documentation

--- doc/example.php
+++ doc/example.php
@@ -1,32 +1,51 @@
 <?php
-
+/**
+ * @author Gerard van Helden <gerard@zicht.nl>
+ * @copyright Zicht online
+ */
 use Zicht\Service\Common\ServiceCallInterface;
 use Zicht\Service\Common\ServiceObserverInterface;
 use Zicht\Service\Common\ServiceWrapper;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-
+// The service we're going to wrap
 class MyService
 {
+    // Some service method.
     public function doIt($name)
     {
+        if (!is_string($name)) {
+            throw new \InvalidArgumentException("Need string!");
+        }
         return sprintf("Hi, %s!\n", $name);
     }
 }
 
+
+// The observer we're going to attach to the service.
 class MyObserver implements ServiceObserverInterface
 {
+    //
     public function notifyBefore(ServiceCallInterface $call)
     {
-        if ($call->getRequest()->getParameterDeep([0]) == 'Smithers') {
-            $call->getResponse()->setResponse(sprintf("%s, eh?\n", $call->getRequest()->getParameterDeep([0])));
-            $call->cancel($this);
+        switch ($call->getRequest()->getParameterDeep([0])) {
+            case 'Smithers':
+                // replace the response with something else.
+                $call->getResponse()->setResponse(sprintf("%s, eh?\n", $call->getRequest()->getParameterDeep([0])));
+
+                // call never gets executed.
+                $call->cancel($this);
+                break;
+            case 'Sideshow Bob':
+                $call->getResponse()->setError(new \Exception("That is no last name!"));
+                break;
         }
     }
 
     public function notifyAfter(ServiceCallInterface $call)
     {
+        fprintf(STDERR, "Call was made: %s(%s)\n", $call->getRequest()->getMethod(), json_encode($call->getRequest()->getParameterDeep([0])));
     }
 
     public function alterRequest(ServiceCallInterface $call)
@@ -41,8 +60,13 @@ class MyObserver implements ServiceObserverInterface
         if ($call->isCancelled()) {
             return;
         }
-        $response = $call->getResponse();
-        $response->setResponse(str_replace('Hi', 'Hello', $response->getResponse()));
+        if ($call->getResponse()->isError()) {
+            $call->getResponse()->setResponse(sprintf("%s :(\n", $call->getResponse()->getError()->getMessage()));
+            $call->getResponse()->setError(null);
+        } else {
+            $response = $call->getResponse();
+            $response->setResponse(str_replace('Hi', 'Hello', $response->getResponse()));
+        }
     }
 }
 
@@ -52,4 +76,5 @@ $wrapper->registerObserver(new MyObserver());
 echo $wrapper->doIt("Bart"); // output: "Hello, Bart Simpson!"
 echo $wrapper->doIt("Lisa"); // output: "Hello, Lisa Simpson!"
 echo $wrapper->doIt("Smithers"); // output: "Smithers, eh?"
-
+echo $wrapper->doIt("Sideshow Bob"); // output: ":("
+echo $wrapper->doIt(1); // output: ":("
