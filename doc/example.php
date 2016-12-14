a35778de3b96288935ffe0d13ca8d43bd947e0ab
Gerard van Helden <gerard@zicht.nl>
2016-12-14 14:50:09 +0100

    more doc

--- doc/example.php
+++ doc/example.php
@@ -6,22 +6,37 @@ use Zicht\Service\Common\ServiceWrapper;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-
+// The service we're going to wrap
 class MyService
 {
+    // Some service method.
     public function doIt($name)
     {
+        if (!is_string($name)) {
+            throw new \InvalidArgumentException("Need string!");
+        }
         return sprintf("Hi, %s!\n", $name);
     }
 }
 
+
+// The observer we're going to attach to the service.
 class MyObserver implements ServiceObserverInterface
 {
+    //
     public function notifyBefore(ServiceCallInterface $call)
     {
-        if ($call->getRequest()->getParameterDeep([0]) == 'Smithers') {
-            $call->getResponse()->setResponse(sprintf("%s, eh?\n", $call->getRequest()->getParameterDeep([0])));
-            $call->cancel($this);
+        switch ($call->getRequest()->getParameterDeep([0])) {
+            case 'Smithers':
+                // replace the response with something else.
+                $call->getResponse()->setResponse(sprintf("%s, eh?\n", $call->getRequest()->getParameterDeep([0])));
+
+                // call never gets executed.
+                $call->cancel($this);
+                break;
+            case 'Sideshow Bob':
+                $call->getResponse()->setError(new \Exception("That is no last name!"));
+                break;
         }
     }
 
@@ -41,8 +56,13 @@ class MyObserver implements ServiceObserverInterface
         if ($call->isCancelled()) {
             return;
         }
-        $response = $call->getResponse();
-        $response->setResponse(str_replace('Hi', 'Hello', $response->getResponse()));
+        if ($call->getResponse()->isError()) {
+            $call->getResponse()->setError(null);
+            $call->getResponse()->setResponse(":(\n");
+        } else {
+            $response = $call->getResponse();
+            $response->setResponse(str_replace('Hi', 'Hello', $response->getResponse()));
+        }
     }
 }
 
@@ -52,4 +72,6 @@ $wrapper->registerObserver(new MyObserver());
 echo $wrapper->doIt("Bart"); // output: "Hello, Bart Simpson!"
 echo $wrapper->doIt("Lisa"); // output: "Hello, Lisa Simpson!"
 echo $wrapper->doIt("Smithers"); // output: "Smithers, eh?"
+echo $wrapper->doIt("Sideshow Bob"); // output: ":("
+echo $wrapper->doIt(1); // output: ":("
 
